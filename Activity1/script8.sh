1420d7ea5b53f4e74d05b8d49c03a6518a4d00f2
Jeanarias22-AA <e.jariasv@ies-sabadell.cat>
2025-04-30 08:30:25 -0400

    Agrego scrip activity1

---
+++ Activity1/script8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo -n "Introduce tu nombre de usuario: "
+read username
+
+echo -n "Introduce tu contraseña (no será visible): "
+read -s password
+echo
+
+echo "Credenciales ingresadas -> Usuario: $username, Contraseña: [oculta]"

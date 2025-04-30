42a7aeddce4076b49c8a780a5601ae062723624f
Jeanarias22-AA <e.jariasv@ies-sabadell.cat>
2025-04-30 14:59:24 +0200

    Update script1.sh

--- Activity2/script1.sh
+++ Activity2/script1.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Pedir número al usuario
 read -p "Introduce un número entero: " num
 
-# Comprobar si es positivo o negativo
 if [ "$num" -gt 0 ]; then
     echo "El número es positivo."
 elif [ "$num" -lt 0 ]; then

d6cdf1e5f7b11598ad9f93a2c6d0b119d752cccf
Jeanarias22-AA <e.jariasv@ies-sabadell.cat>
2025-05-30 12:02:19 +0200

    Update ejecuta_para_programar_cron.sh

--- Activity4/ejecuta_para_programar_cron.sh
+++ Activity4/ejecuta_para_programar_cron.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# ESTE SCRIPT FUNCIONARA EN LA MAQUINA LOCAL, PARA GENERAR UN AUTOMATIZACION EN EL SCRIPT DE MONITORIZACIONV
+# Y SE EJECUTE CADA HORA:
+
+DIRECTORIO="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CRON="0 * * * * $DIRECTORIOL/monitorizacion.sh --auto >> $DIRECTORIO/monitor.log 2>&1" # PERMITE EJECURAR EL SCRIPT CADA HORA
+
+(crontab -l | grep -Fv "$CRON"; echo "$CRON") | crontab - # AÑADIMOS LA ENTRADA CRONTAB SIN DUPLICARLA DE FORMA AUTOMATICA
+
+echo "Cron configurado para ejecutarse cada hora desde $DIRECTORIO"
 

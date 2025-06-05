4e16ac1a991fc3c00caa4763cf5f69d410d118fc
Jeanarias22-AA <e.jariasv@ies-sabadell.cat>
2025-06-05 15:34:11 +0200

    Update scrpit9a&b.sh

--- Activity1/scrpit9a&b.sh
+++ Activity1/scrpit9a&b.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+CRACK="oriolorioloriol"
+
+# Pregunta A
+
+# Quita la coincidencia más larga desde el inicio
+echo "Resultado de ##*ori: ${CRACK##*ori}"
+
+# Quita la coincidencia más corta desde el inicio
+echo "Resultado de #*ori: ${CRACK#*ori}"
+
+# Quita la coincidencia más larga desde el final
+echo "Resultado de %%ol*: ${CRACK%%ol*}"
+
+# Quita la coincidencia más corta desde el final
+echo "Resultado de %ori*: ${CRACK%ori*}"
+
+# Pregunta B
+# Estas técnicas permiten modificar textos fácilmente en Bash.
